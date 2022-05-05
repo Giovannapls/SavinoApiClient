68f33fa7229aff8a82275989d527cd02f0f3d7a3
Giovannapls <giovanna.polissici@mercadolivre.com>
2022-05-05 02:12:46 -0300

    [Add] list movie

---
+++ Client/moviesApp/src/app/models/movie.ts
@@ -0,0 +1,8 @@
+export class movie{
+    name: string;
+    base64Image:string;
+    description:string;
+    genre:string;
+    author:string;
+    note:number;
+}
--- Client/moviesApp/src/app/tab2/tab2.page.ts
+++ Client/moviesApp/src/app/tab2/tab2.page.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { director } from '../models/director';
+import { movie } from '../models/movie';
 
 @Component({
-  selector: 'app-tab2',
+  selector: 'teste',
   templateUrl: 'tab2.page.html',
 })
 export class Tab2Page {
-  
-
+  moviesRequest: Observable<any>;
+  listaFilmes: Array<movie> = new Array<movie>();
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+    
+    this.moviesRequest = this.httpClient.get('http://localhost:5000/api/movies/ConsultarFilmes')
+    this.moviesRequest
+    .subscribe(data => {
+      this.listaFilmes = data
+    })
+}
 }
 
